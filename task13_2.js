2da09be46b28aa55acdd391c638fe408b705e509
Samia <samiaazme213@gmail.com>
2026-01-30 01:38:19 +0600

    second task

---
+++ task13_2.js
@@ -0,0 +1,9 @@
+let Mathematics = 75.25;
+let Biology = 65;
+let Chemistry = 80;
+let Physics = 35.45;
+let Bangla = 99.50;
+
+var avg = (Mathematics+Biology+Chemistry+Physics+Bangla)/5;
+
+console.log(avg.toFixed(2));
