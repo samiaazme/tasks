f9318ba28912a3a221ed9cebf57bd40b129cd75f
samiaazme <samiaazme213@gmail.com>
2026-01-30 15:34:23 +0600

    tasksss

---
+++ task14_1.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let money = 700;
+
+if (money > 500)
+{
+    console.log('Free Coke')
+}
+else{
+    console.log('Coke : 30tk')
+}
