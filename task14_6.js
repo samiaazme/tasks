311ed4f17bf18dd2b40d3b00ed74f91adce2ac97
samiaazme <samiaazme213@gmail.com>
2026-01-31 00:10:16 +0600

    ternary created

--- task14_6.js
+++ task14_6.js
@@ -8,27 +8,28 @@ Ticket fare Calculator
 */
 
 let ticketFare = 800;
-let age = 2;
-let student = true;
+let age = 61;
+let student = false;
 
-if (age < 10) {
-  console.log("Free");
-} else if (age >= 60) {
-  let discountFare = ticketFare - (ticketFare * 15) / 100;
-  console.log("Get 15% Discount", discountFare);
-} else if (student === true) {
-  let discountFare = ticketFare - (ticketFare * 50) / 100;
-  console.log("Get 50% discount", discountFare);
-} else {
-  console.log("Regular ticket fare 800 tk");
-}
+// if (age < 10) {
+//   console.log("Free");
+// } else if (age >= 60) {
+//   let discountFare = ticketFare - (ticketFare * 15) / 100;
+//   console.log("Get 15% Discount", discountFare);
+// } else if (student === true) {
+//   let discountFare = ticketFare - (ticketFare * 50) / 100;
+//   console.log("Get 50% discount", discountFare);
+// } else {
+//   console.log("Regular ticket fare 800 tk");
+// }
 
 // ternary operator:
-
-age < 10
-  ? "free"
-  : age >= 60
-    ? "Get 15% Discount"
-    : student === true
-      ? "Get 50% discount"
-      : "Regular ticket fare 800 tk";
+let msg =
+  age < 10
+    ? "free"
+    : age >= 60
+      ? "Get 15% Discount " + (ticketFare - (ticketFare * 15) / 100) 
+      : student === true
+        ? "Get 50% discount " + (ticketFare - (ticketFare * 50) / 100)
+        : "Regular ticket fare 800 tk";
+console.log (msg);
